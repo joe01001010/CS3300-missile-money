9398988c046c8375d402c75c5f7be4b81b31e700
Joe Weibel <jweibel@uccs.edu>
2025-10-18 15:06:29 -0600

    Added a middleware that forces the user to logout after 15 minutes. This is a change because the previous implementation would only log the user out on their next request

--- missile_money/missile_money/settings.py
+++ missile_money/missile_money/settings.py
@@ -131,6 +131,10 @@ LOGOUT_REDIRECT_URL = '/'
 LOGIN_URL = '/accounts/login/'
 # Set session expiry time to 15 minutes
 # The session cookie will reset when the user makes a request
+MIDDLEWARE = [
+    'main.middleware.auto_logout.AutoLogout',
+]
+AUTO_LOGOUT_DELAY = 15 * 60
 SESSION_COOKIE_AGE = 15 * 60
 SESSION_SAVE_EVERY_REQUEST = True
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
